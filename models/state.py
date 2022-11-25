ddcbd2a9b1ae339c743737c18f83a395ae6cb4f8
charlespaul1 <charlespaulwabomba@gmail.com>
2022-11-25 11:16:09 +0300

    creating the state class

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+creatinig the sate class
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    defining the state class
+    """
+    name = ""
