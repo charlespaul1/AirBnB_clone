96fb8d79318c16f72c5d707b6ea403396b5ce819
charlespaul1 <charlespaulwabomba@gmail.com>
2022-11-25 11:21:43 +0300

    creating the city class

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+defining the class City
+"""
+
+from models.base_model import BaseModel
+class City(BaseModel):
+    """
+    definition of the class City that inherits from BaseModel
+    """
+    state_id = ""
+    name = ""
